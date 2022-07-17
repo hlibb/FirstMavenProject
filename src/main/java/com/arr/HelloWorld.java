917a09d1f4f0266fce49522f886a10ec73b6b71a
hlibb <Astashev01@gmail.com>
2022-07-17 12:08:52 +0200

    + lombook

--- src/main/java/com/arr/HelloWorld.java
+++ src/main/java/com/arr/HelloWorld.java
@@ -1,13 +1,12 @@
 package com.arr;
 
+import lombok.Data;
+
+@Data
 public class HelloWorld {
     String hello;
 
     public HelloWorld() {
         this.hello = "Hello World!";
-    }
-
-    public String getHello() {
-        return hello;
     }
 }
